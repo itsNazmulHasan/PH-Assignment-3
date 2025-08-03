f49cc18d03b2e215c415a42f61e360544f8963bb
Nazmul Hasan <personalmail.nazmulhasan@gmail.com>
2025-08-03 23:46:33 +0600

    problem 3

--- final.js
+++ final.js
@@ -7,4 +7,20 @@ var eachSonsShare = area/2;
 console.log(eachSonsShare);
 
 
+/** Problem -02 ( Cycle or Laptop ) */
+var money =10000;
+//write your code here
 
+if (money >= 25000)
+{
+    console.log("Laptop");
+    
+} 
+else if (money >= 10000)
+{
+        console.log("Cycle");
+        
+}
+else  {
+    console.log("Chocolate");
+    }
